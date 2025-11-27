4f0266e8001554e68710cc336d6aa38e7a5368be
Your Name <your-email@example.com>
2025-11-28 00:33:07 +0500

    Adding Order_Service, Product_Service and Category_Service interfaces

---
+++ Grocery_Store-main/BusinessLogic/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+using WebApplication4.Entities;
+
+namespace WebApplication4.BusinessLogic.Services
+{
+    public interface IOrderService
+    {
+        int GenerateOrderId();
+        Order CreateOrder(string userId, string fullName, string address, string city, string state, string zipCode, decimal total);
+        void ProcessOrderItems(int orderId, int[] productIds, int[] quantities, decimal[] prices);
+        List<Order> GetUserOrders(string userId);
+    }
+}
+
