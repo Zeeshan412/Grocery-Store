451a55e4a65e802e43c302d2c88d7728ae3abeb2
Ahmad Sadeed Khan <ahmadsadeed568@gmail.com>
2025-11-29 20:19:13 +0500

    Added AppSettings files

--- program.cs
+++ program.cs
@@ -40,17 +40,6 @@ DatabaseInitializer.InitializeDatabase(connectionString);
 // Seed sample data for testing (only if database is empty)
 SampleDataSeeder.SeedSampleData(connectionString);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
